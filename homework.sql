a080eab71ea1625b9e4efe637c40f611d3843afa
Thoma02 <122801995+Thoma02@users.noreply.github.com>
2023-02-16 12:27:04 +0000

    Homework commit

--- homework.sql
+++ homework.sql
@@ -5,65 +5,96 @@ USE hr;
 -- 1. Find all employees who work in department with id `90`
 SELECT first_name,
   last_name,
-  department_name
+  department_id
 FROM employees
-WHERE department_id 
+WHERE department_id = '90';
+
 
 -- @block
-  -- 2. Find all employees who earn more than `5000`
+-- 2. Find all employees who earn more than `5000`
 SELECT first_name,
   last_name,
   salary
 FROM employees
-WHERE salary > 5000 
+WHERE salary > 5000;
+
 
 -- @block
-  -- 3. Find all employees whose surnames starts with `L`
-SELECT first_name, last_name
+-- 3. Find all employees whose surnames starts with `L`
+SELECT first_name,
+  last_name
 FROM employees
-WHERE last_name LIKE 'a%'
+WHERE last_name LIKE 'L%';
+
 
 -- @block
 -- 4. Find all departments that location_id equals `1700`
-SELECT city, location_id
-FROM locations
-WHERE location_id = 1700
+SELECT department_name,
+  location_id
+FROM departments
+WHERE location_id = 1700;
 
 
 -- @block
 -- 5. Find all towns with country_id equals `US`
-SELECT country_id
+SELECT city,
+  country_id
 FROM locations
-WHERE country_id LIKE '%US'
+WHERE country_id = 'US';
 
 
 -- @block
 -- 6. Print salary of employee
 -- name: `Lex`
 -- surname: `De Haan`
-SELECT;
+SELECT salary
+FROM employees
+WHERE first_name = 'Lex'
+  AND last_name = 'De Haan';
 
 
 -- @block
 -- 7. Find all employees whose job_id equals `FI_ACCOUNT` and they earn less than `8000`
-SELECT;
+SELECT first_name,
+  last_name,
+  job_id,
+  salary
+FROM employees
+WHERE job_id = 'FI_ACCOUNT'
+  AND salary < 8000;
 
 
 -- @block
 -- 8. Find all employees whose surnames in the middle of the word contain combinations of `kk` or `ll`
-SELECT;
+SELECT first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE '%kk%'
+  OR LIKE '%ll%';
 
 
 -- @block
 -- 9. Find employees with commission_pct NULL
-SELECT;
+SELECT first_name,
+  last_name,
+  commission_pct
+FROM employees
+WHERE commission_pct = IS NULL;
 
 
 -- @block
 -- 10. Find all employees except those who work in departments `80` and `110`
-SELECT;
+SELECT first_name,
+  last_name,
+  department_id
+FROM employees
+WHERE department_id NOT IN (80, 110);
 
 
 -- @block
 -- 11. Find all employees who earns from 5000 to 7000 (including ends)
-SELECT;
+SELECT first_name,
+  last_name,
+  salary
+FROM employees
+WHERE salary BETWEEN 5000 AND 7000;
